2dfbf22932ca400f95ed9e3ec0c7636dcc0c46fd
Dias <diasakbergen2205@gmail.com>
2025-11-03 23:18:46 +0500

    Snapkit update

--- firstapp/ViewController.swift
+++ firstapp/ViewController.swift
@@ -6,61 +6,158 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var Label: UILabel!
     
-    @IBOutlet weak var textfield2: UITextField!
-    @IBOutlet weak var textfield: UITextField!
+    lazy var TargetPrice: UITextField = {
+        let targetPrice = UITextField()
+        targetPrice.placeholder = "0"
+        targetPrice.borderStyle = .roundedRect
+        targetPrice.keyboardType = .numberPad
+        return targetPrice
+    }()
+    
+    lazy var MonthlySaving: UITextField = {
+        let monthlySaving = UITextField()
+        monthlySaving.placeholder = "0"
+        monthlySaving.borderStyle = .roundedRect
+        monthlySaving.keyboardType = .numberPad
+        return monthlySaving
+    }()
+    
+    lazy var Savings: UITextField = {
+        let savings = UITextField()
+        savings.placeholder = "0"
+        savings.borderStyle = .roundedRect
+        savings.keyboardType = .numberPad
+        return savings
+    }()
+    
+    lazy var Label: UILabel = {
+        let label = UILabel()
+        label.text = "My Big Purchase"
+        label.font = .systemFont(ofSize: 30)
+        return label
+    }()
+    
+    lazy var TargetPriceLabel: UILabel = {
+        let largetPriceLabel = UILabel()
+        largetPriceLabel.text = "Target Price:"
+        largetPriceLabel.font = .systemFont(ofSize: 15)
+        return largetPriceLabel
+    }()
+    lazy var MonthlySavingLabel: UILabel = {
+        let monthlySavingLabel = UILabel()
+        monthlySavingLabel.text = "Monthly Savings:"
+        monthlySavingLabel.font = .systemFont(ofSize: 15)
+        return monthlySavingLabel
+    }()
+    lazy var SavingsLabel: UILabel = {
+        let savingsLabel = UILabel()
+        savingsLabel.text = "Savings:"
+        savingsLabel.font = .systemFont(ofSize: 15)
+        return savingsLabel
+    }()
+    
+    lazy var ResultLabel: UILabel = {
+        let resultLabel = UILabel()
+        resultLabel.text = "Result:"
+        resultLabel.font = .systemFont(ofSize: 15)
+        return resultLabel
+    }()
+    
+    lazy var CalculateButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Calculate", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .systemBlue
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        SetupUI()
+        
     }
 
-    @IBAction func touchDown(_ sender: Any) {
-        //Label.text = textfield.text
-        let a = textfield.text!
+    func SetupUI() {
+        view.addSubview(Label)
+        view.addSubview(TargetPriceLabel)
+        view.addSubview(TargetPrice)
+        view.addSubview(MonthlySavingLabel)
+        view.addSubview(MonthlySaving)
+        view.addSubview(SavingsLabel)
+        view.addSubview(Savings)
+        view.addSubview(CalculateButton)
+        view.addSubview(ResultLabel)
         
-        let b = textfield2.text!
-        
-        let sum = Int(a)! + Int(b)!
-        
-        Label.text = "sum = \(sum)"
         
         
+        Label.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(30)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(40)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
         
+        // TARGET PRICE
+        TargetPriceLabel.snp.makeConstraints { make in
+            make.top.equalTo(Label.snp.bottom).offset(40)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(40)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+        TargetPrice.snp.makeConstraints { make in
+            make.top.equalTo(TargetPriceLabel.snp.bottom).offset(20)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(40)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+        
+        // MONTHLY SAVING
+        MonthlySavingLabel.snp.makeConstraints { make in
+            make.top.equalTo(TargetPrice.snp.bottom).offset(40)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(40)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+        MonthlySaving.snp.makeConstraints { make in
+            make.top.equalTo(MonthlySavingLabel.snp.bottom).offset(20)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(40)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+        
+        // SAVINGS
+        SavingsLabel.snp.makeConstraints { make in
+            make.top.equalTo(MonthlySaving.snp.bottom).offset(40)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(40)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+        Savings.snp.makeConstraints { make in
+            make.top.equalTo(SavingsLabel.snp.bottom).offset(20)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(40)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+
+        CalculateButton.snp.makeConstraints { make in
+            make.top.equalTo(Savings.snp.bottom).offset(40)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(40)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+        CalculateButton.addTarget(self, action: #selector(calculate), for: .touchUpInside)
+        
+        ResultLabel.snp.makeConstraints { make in
+            make.top.equalTo(CalculateButton.snp.bottom).offset(20)
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(40)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
     }
-    @IBAction func Touchdown4(_ sender: Any) {
-        
-        let a = textfield.text!
-        
-        let b = textfield2.text!
-        
-        let multiply = Int(a)! * Int(b)!
-        
-        Label.text = "multiply = \(multiply)"
+    @objc func calculate() {
+        let a = TargetPrice.text!
+        let b = MonthlySaving.text!
+        let c = Savings.text!
+        let result = (Int(a)! - Int(c)!) / Int(b)!
+        ResultLabel.text = String(result)
     }
-    @IBAction func Touchdown3(_ sender: Any) {
-        
-        let a = textfield.text!
-        
-        let b = textfield2.text!
-        
-        let divide = Int(a)! / Int(b)!
-        
-        Label.text = "divide = \(divide)"
-    }
-    @IBAction func Touchdown2(_ sender: Any) {
-        
-        let a = textfield.text!
-        
-        let b = textfield2.text!
-        
-        let subtract = Int(a)! - Int(b)!
-        
-        Label.text = "subtract = \(subtract)"
-    }
-    
-    
+
 }
 
